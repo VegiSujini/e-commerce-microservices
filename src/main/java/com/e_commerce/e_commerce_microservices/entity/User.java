b5a5a371caae929dc5918a1f6fc09e411e557c65
VegiSujini <sujini.vegi@gmail.com>
2024-08-03 11:56:49 +0530

    added user entity

---
+++ src/main/java/com/e_commerce/e_commerce_microservices/entity/User.java
@@ -0,0 +1,32 @@
+package com.e_commerce.e_commerce_microservices.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private boolean mfaEnabled;
+    private String otp;
+    private LocalDateTime otpExpiration;
+}
