58a6fc96a45e521df76cc9b9981a0949e0d9822c
VegiSujini <sujini.vegi@gmail.com>
2024-08-03 11:59:19 +0530

    added user repository

---
+++ src/main/java/com/e_commerce/e_commerce_microservices/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_commerce_microservices.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.e_commerce.e_commerce_microservices.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
