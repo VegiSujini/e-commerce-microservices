d4fb8a7b5abd6e6a4e937105a69620af21434591
VegiSujini <sujini.vegi@gmail.com>
2024-08-03 11:41:21 +0530

    initial commit

---
+++ src/main/java/com/e_commerce/e_commerce_microservices/ECommerceMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_commerce_microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e_commerce/e_commerce_microservices/ECommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.e_commerce_microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
